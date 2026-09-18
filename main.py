5634e2f7d44023143c7e606b6a1719d37642bfb0
Reinhart <196166520+Reinhart-py@users.noreply.github.com>
2026-09-18 14:12:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,16 @@
 import sys
-
-from gmaps.runner import GMapsRunner
-from runner import Runner as TwoGISRunner
-from utils.cli_parser import initiate_cli_parser
-
+from gui.app import JiyaApp
 
 def main() -> None:
     try:
-        config = initiate_cli_parser()
-        if config.engine == "2gis":
-            config.start_page = config.start_step
-            runner = TwoGISRunner(config=config)
-            runner.run()
-        elif config.engine == "gmaps":
-            runner = GMapsRunner(
-                target_input=config.target,
-                output_path=config.output_path,
-                start_index=config.start_step,
-                target_count=config.target_count,
-            )
-            runner.run()
+        app = JiyaApp()
+        app.mainloop()
     except KeyboardInterrupt:
-        print("\nProcess terminated gracefully.")
+        print("\nApplication closed by user.")
         sys.exit(0)
-
+    except Exception as e:
+        print(f"Fatal UI error: {e}")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
